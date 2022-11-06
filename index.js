7fd843737461229a1dffb7be1c02ba292d600624
Lukas Rybacki <kontakt@icepole.de>
2022-11-06 20:21:39 +0100

    Update: remove rimraf and add function call and exports to module

--- index.js
+++ index.js
@@ -18,11 +18,27 @@ function rimrafNodeModules(path) {
 	})
 }
 
-try {
-	printResponse(await bundle.bundleAndSync(targetPath, destinationPath))
-	printResponse(`Next: clean up all unnecessary node_modules in ${destinationPath}`)
-	rimrafNodeModules(destinationPath)
-	printResponse(await zipper.zip(bundleZip))
-} catch (e) {
-	console.error(e)
+function cleanup() {
+	if (fs.existsSync('dist')) {
+		fs.rmSync('dist', {recursive: true})
+	}
+
+	if (fs.existsSync(bundleZip)) {
+		fs.rmSync(bundleZip)
+	}
+
+	console.info(`All clean here`)
 }
+
+async function run() {
+	try {
+		printResponse(await bundle.bundleAndSync(targetPath, destinationPath))
+		printResponse(`Next: clean up all unnecessary node_modules in ${destinationPath}`)
+		rimrafNodeModules(destinationPath)
+		printResponse(await zipper.zip(bundleZip))
+	} catch (e) {
+		console.error(e)
+	}
+}
+
+export { cleanup, run }
